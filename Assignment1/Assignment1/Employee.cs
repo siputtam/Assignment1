4d03198c4f768d98085d8db8d90c68669868cb9d
Uttam Kumar Das <59952873+siputtam@users.noreply.github.com>
2020-01-27 15:12:31 +0530

    Add files via upload

---
+++ Assignment1/Assignment1/Employee.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Reflection;
+
+namespace Assignment1
+{
+    public class Employee
+    {
+        public delegate void ActionTaken(string[] msg, int i);
+        public event ActionTaken Action;
+        public string[] s = new string[3];
+        public int i;
+        public void ActionPerformed()
+        {
+            Action(s, i);
+        }
+        private int Id;
+        private string emp_name;
+        private string dept_name;
+        public Employee(int Id, string emp_name, string dept_name)
+        {
+            this.Id = Id;
+            this.emp_name = emp_name;
+            this.dept_name = dept_name;
+        }
+        public int GetId(Employee E1)
+        {
+            i = 0;
+            s[0] = MethodBase.GetCurrentMethod().Name;
+            E1.Action += new Employee.ActionTaken(Action_Shown);
+            E1.ActionPerformed();
+            return this.Id;
+
+        }
+        public string GetName(Employee E1)
+        {
+            i = 1;
+            s[1] = MethodBase.GetCurrentMethod().Name;
+            E1.Action += new Employee.ActionTaken(Action_Shown);
+            E1.Action -= new Employee.ActionTaken(Action_Shown);
+            E1.ActionPerformed();
+            return this.emp_name;
+        }
+        public string GetDepartmentName(Employee E1)
+        {
+            i = 2;
+            s[2] = MethodBase.GetCurrentMethod().Name;
+            E1.Action += new Employee.ActionTaken(Action_Shown);
+            E1.Action -= new Employee.ActionTaken(Action_Shown);
+            E1.ActionPerformed();
+            return this.dept_name;
+        }
+        static void Action_Shown(string[] msg, int i)
+        {
+            Console.WriteLine(msg[i] + "  method called");
+        }
+        public void set(int uid)
+        {
+            Id = uid;
+            Console.WriteLine("Updated Id: " + Id);
+
+        }
+        public void set(string name)
+        {
+            emp_name = name;
+            Console.WriteLine("Updated employee name:" + emp_name);
+        }
+        public void set(string name, int Id)
+        {
+            dept_name = name;
+            Console.WriteLine("Updated Department Name:" + dept_name);
+        }
+    }
+}
---
+++ Assignment1/Assignment1/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Reflection;
+
+namespace Assignment1
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Enter the employee ID");
+            int Id = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter the Name of Employee");
+            string e_name = Console.ReadLine();
+            Console.WriteLine("Enter Department of Employee");
+            string d_name = Console.ReadLine();
+            Employee E1 = new Employee(Id, e_name, d_name);
+            int id = E1.GetId(E1);
+            Console.WriteLine("Employee ID:" + id);
+            string ename = E1.GetName(E1);
+            Console.WriteLine("Employee Name:" + ename);
+            string dname = E1.GetDepartmentName(E1);
+            Console.WriteLine("Department Name Of Employee:" + dname);
+            Console.WriteLine("Enter Id to Update ");
+            int update_id = Convert.ToInt32(Console.ReadLine());
+            E1.set(update_id);
+            Console.WriteLine("Enter name of employee to Update ");
+            string update_ename = Console.ReadLine();
+            E1.set(update_ename);
+            Console.WriteLine("Enter Department name to Update ");
+            string update_dname = Console.ReadLine();
+            E1.set(update_dname, 0);
+
+        }
+
+
+    }
+}
